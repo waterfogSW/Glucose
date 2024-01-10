61ade62f6369c1b64ca158f5f5b4700f5da042eb
San Kim <waterfog9580@gmail.com>
2024-01-10 14:29:02 +0900

    :heavy_plus_sign: jjwt api, impl 의존성 추가

--- common/jwt/build.gradle.kts
+++ common/jwt/build.gradle.kts
@@ -1,3 +1,5 @@
- dependencies {
+dependencies {
     implementation("io.jsonwebtoken:jjwt:${Version.JJWT}")
+    implementation("io.jsonwebtoken:jjwt-api:${Version.JJWT}")
+    implementation("io.jsonwebtoken:jjwt-impl:${Version.JJWT}")
 }
